feca535a06dbfb3bff23e2327f1fce0b8b359409
nqmetke <nqmetke@gmail.com>
2016-06-23 16:34:05 -0400

    Last commit didnt work. Hope this fixes it

--- public/js/landingParallax.js
+++ public/js/landingParallax.js
@@ -2,7 +2,7 @@
  * Created by nqmet on 6/23/2016.
  */
 var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
-new ScrollMagic.Scene({triggerElement: ".headerImage"})
-    .setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+new ScrollMagic.Scene({triggerElement: ".header-image"})
+    .setTween(".header-image > div", {y: "80%", ease: Linear.easeNone})
     .addIndicators()
     .addTo(controller);
