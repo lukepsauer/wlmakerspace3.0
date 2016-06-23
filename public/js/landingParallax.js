3b4b9276ab8fc71a08a6f80035133ea6bd21f4b5
nqmetke <nqmetke@gmail.com>
2016-06-23 16:48:31 -0400

    The last commit is a lie.

--- public/js/landingParallax.js
+++ public/js/landingParallax.js
@@ -2,7 +2,10 @@
  * Created by nqmet on 6/23/2016.
  */
 var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
-new ScrollMagic.Scene({triggerElement: ".header-image"})
-    .setTween(".header-image > div", {y: "80%", ease: Linear.easeNone})
+new ScrollMagic.Scene({triggerElement: ".img1"})
+    .setTween(".img1 > div", {y: "80%", ease: Linear.easeNone})
     .addIndicators()
     .addTo(controller);
+
+
+console.log("Is this thing on?");
